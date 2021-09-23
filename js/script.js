e990435b35631edc903ed3a8328a1a37abd322ad
Bensalem Haithem <42184885+Haithem-BEN@users.noreply.github.com>
2021-09-23 15:48:37 +0100

    Eefactoring Hamburger menu

--- js/script.js
+++ js/script.js
@@ -3,9 +3,5 @@
 const btnHamburgerEle = document.querySelector('.btnHamburger')
 const hamburgerMenu = document.querySelector('.header__mobile')
 btnHamburgerEle.addEventListener('click',(e)=>{
-    if (hamburgerMenu.classList.contains('open')){
-        hamburgerMenu.classList.remove('open')
-    } else{
-        hamburgerMenu.classList.add('open')
-    } 
+    hamburgerMenu.classList.toggle('open') 
 })
